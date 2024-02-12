46c3804358bf1e3c53b261534c883e407f4e766c
Wasya Rubaniy <157049889+Wasya-01@users.noreply.github.com>
2024-02-12 17:56:26 +0200

    Delete JS_3HW directory

--- JS_3HW/script.js
+++
@@ -1,9 +0,0 @@
-var userName = prompt("Будь ласка, введіть своє ім'я:");
-
-if (userName !== null && userName.trim() !== "") {
- 
-    alert("Привіт, " + userName + "!");
-} else {
-
-    alert("Введіть коректне ім'я.");
-}
